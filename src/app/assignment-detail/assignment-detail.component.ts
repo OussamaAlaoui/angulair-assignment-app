ff9cec6444ae4e523d20b6dc8cef2c30fa2342c5
OussamaAlaoui <ousalis99@gmail.com>
2022-01-28 10:52:29 +0100

    add queryparams, fragment and recover these parameters from the url

--- src/app/assignment-detail/assignment-detail.component.ts
+++ src/app/assignment-detail/assignment-detail.component.ts
@@ -6,33 +6,56 @@ import { AssignmentsService } from '../shared/assignments.service';
 @Component({
   selector: 'app-assignment-detail',
   templateUrl: './assignment-detail.component.html',
-  styleUrls: ['./assignment-detail.component.css']
+  styleUrls: ['./assignment-detail.component.css'],
 })
 export class AssignmentDetailComponent implements OnInit {
   // @Input() transmittedAssignment?:Assignment;
   @Output() deleteAssignmentEvent = new EventEmitter<Assignment>();
-  transmittedAssignment?: Assignment
+  transmittedAssignment?: Assignment;
 
-  constructor(private assignmentService: AssignmentsService, private route: ActivatedRoute, private router: Router) { }
+  constructor(
+    private assignmentService: AssignmentsService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
   assignmentDelete?: Assignment = undefined;
   ngOnInit(): void {
     // console.log("Transmitted Assignment= "+ this.transmittedAssignment)
     const id: number = +this.route.snapshot.params['id'];
-    console.log("id=" + id);
+    console.log('id=' + id);
 
-    this.assignmentService.getAssignmentById(id).subscribe(assignment => this.transmittedAssignment = assignment);
+    this.assignmentService
+      .getAssignmentById(id)
+      .subscribe((assignment) => (this.transmittedAssignment = assignment));
   }
   checkAssignment() {
     if (this.transmittedAssignment != null)
-      this.assignmentService.updateAssignment(this.transmittedAssignment).subscribe(message => { console.log(message); 
-      this.router.navigate(["/home"])});
+      this.assignmentService
+        .updateAssignment(this.transmittedAssignment)
+        .subscribe((message) => {
+          console.log(message);
+          this.router.navigate(['/home']);
+        });
     // this.transmittedAssignment.due=true;
   }
-  onDelete(assignment:Assignment){
-    this.assignmentService.deleteAssignment(assignment).subscribe(message => {
+  onDelete(assignment: Assignment) {
+    this.assignmentService.deleteAssignment(assignment).subscribe((message) => {
       console.log(message);
-      this.router.navigate(["/home"])
+      this.router.navigate(['/home']);
+    });
+  }
 
-    })
+  onClickEdit() {
+    this.router.navigate(
+      ['/assignment', this.transmittedAssignment?.id, 'edit'],
+      {
+        queryParams: {
+          name: 'Oussama',
+          lastname: 'Alaoui',
+          debug: true,
+        },
+        fragment: 'edition',
+      }
+    );
   }
 }
