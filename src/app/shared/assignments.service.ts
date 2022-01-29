35a1cc45d8848c3b42f349f110c4c50611588e2e
OussamaAlaoui <ousalis99@gmail.com>
2022-01-29 11:04:08 +0100

    add express and server.js to deploy the frontend in Heroku

--- src/app/shared/assignments.service.ts
+++ src/app/shared/assignments.service.ts
@@ -10,31 +10,36 @@ import { Assignment } from '../assignments/assignment.model';
 // a service manage data
 export class AssignmentsService {
   assignments: Assignment[] = [];
-  url ="http://localhost:8010/api/assignments";
-  constructor(private http:HttpClient) {}
+  // url ="http://localhost:8010/api/assignments";
+  url = 'https://my-first-angular-website.herokuapp.com/api/assignments';
+  constructor(private http: HttpClient) {}
   // to send the list of assignments
   getAssignments(): Observable<Assignment[]> {
     // in case we are sending a request in the cloud and the server is send a request to the db so we don;t know how much time will it take
     // so we will not send the data but we will send an object "Observable"
     // observable function
-      
 
-  return  this.http.get<Assignment[]>(this.url).pipe(map(_ => { console.log("hello",_); return _;}) ,catchError(this.handleError<any>("!!! you can't get all the daata"))) 
-
+    return this.http.get<Assignment[]>(this.url).pipe(
+      map((_) => {
+        console.log('hello', _);
+        return _;
+      }),
+      catchError(this.handleError<any>("!!! you can't get all the daata"))
+    );
   }
   addAssignment(assignment: Assignment): Observable<any> {
     //console.log("Adding an assignment through service ....")
-// console.log(assignment);
-    return this.http.post(this.url,assignment);
-  // this.assignments.push(assignment);
+    // console.log(assignment);
+    return this.http.post(this.url, assignment);
+    // this.assignments.push(assignment);
     // return of('The assignment is added successfully');
   }
 
   updateAssignment(assignment: Assignment): Observable<any> {
     //console.log("updating an assignment through service ....")
     // assignment.name=
-// console.log("---------------------"+assignment);
-    return this.http.put(this.url,assignment);
+    // console.log("---------------------"+assignment);
+    return this.http.put(this.url, assignment);
     // assignment.due = true;
     // return of('the assignment has been updated successfully');
   }
@@ -42,30 +47,36 @@ export class AssignmentsService {
   deleteAssignment(assignment: Assignment): Observable<any> {
     //console.log("Deleting an assignment through service ....")
     // this.assignments.splice(this.assignments.indexOf(assignment), 1);
-    return this.http.delete(this.url + "/"+ assignment._id);
+    return this.http.delete(this.url + '/' + assignment._id);
     // return of('The assignment has been deleted successfully!!');
   }
 
   getAssignmentById(id: number): Observable<Assignment | undefined> {
     // let assignment = this.assignments.find((a) => a.id == id);
-    return this.http.get<Assignment>(this.url+"/"+id)
-    .pipe(map (a => {
-        a.name +=" passed by pipe";
+    return this.http.get<Assignment>(this.url + '/' + id).pipe(
+      map((a) => {
+        a.name += ' passed by pipe';
         return a;
       }),
-      catchError(this.handleError<any>('### catcheError : getassignments by id avec id='+id+" doesn't work"))
+      catchError(
+        this.handleError<any>(
+          '### catcheError : getassignments by id avec id=' +
+            id +
+            " doesn't work"
+        )
+      )
     );
 
     // return of(assignment);
   }
-private handleError<T>(operation: any, result?: T) {
-   return (error: any): Observable<T> => {
-     console.log(error); // pour afficher dans la console
-     console.log(operation + ' a échoué ' + error.message);
+  private handleError<T>(operation: any, result?: T) {
+    return (error: any): Observable<T> => {
+      console.log(error); // pour afficher dans la console
+      console.log(operation + ' a échoué ' + error.message);
 
-     return of(result as T);
-   }
-};
+      return of(result as T);
+    };
+  }
 
   // updateAssignmentData(assignment: Assignment): Observable<string> {
   //   //console.log("The assignment data has been updated");
